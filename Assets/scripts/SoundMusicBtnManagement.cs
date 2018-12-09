b65f8d99398931ca047bf0fa65e8b3e71b1d4092
Unknown <nhcung@knox.edu>
2018-12-09 16:01:16 -0600

    Addy animation completed

---
+++ Assets/scripts/SoundMusicBtnManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundMusicBtnManagement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SceneManager.GetActiveScene().name == "Main" || SceneManager.GetActiveScene().name =="FromLevelLoadFile"){
+            Destroy(gameObject);
+        }
+	}
+}
